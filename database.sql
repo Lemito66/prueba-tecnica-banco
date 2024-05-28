fdadc95d1d8761738305e79287deeeb8a7858426
Lemito66 <lemito66@gmail.com>
2024-05-28 15:30:30 -0500

    añadiendo el salario más alto

--- database.sql
+++ database.sql
@@ -25,3 +25,28 @@ select * from clientes
 delete from clientes
 
 select salary, case when salary > 4500 then 'alto' 
+
+-- añade una columna que contenga el salario anual
+alter table clientes
+add annual_salary as salary * 12
+
+-- añade una columna que contenga el rango salarial (BAJO, MEDIO, ALTO)
+alter table clientes
+add salary_range 
+as case 
+	when salary * 12 between 0 and 10000 then 'BAJO'
+	when salary * 12 between 10001 and 30000 then 'MEDIO'
+	when salary * 12 > 30000 then 'ALTO'
+end
+
+-- Obtener la cantidad de clientes por rango salarial
+select count(*) as cantidad, salary_range
+from clientes
+group by salary_range
+
+
+-- Obtener el salario anual más alto
+select top 1 annual_salary, id 
+from clientes
+group by  annual_salary, id
+order by annual_salary desc
